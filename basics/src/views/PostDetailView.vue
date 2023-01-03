cefca05ea009710f0b9d33e0b3eb7a16384e4ef7
lucassantos22 <lucas.afsantos@hotmail.com>
2023-01-03 16:11:30 -0300

    chore: vue-router

---
+++ basics/src/views/PostDetailView.vue
@@ -0,0 +1,35 @@
+<script setup>
+</script>
+
+<template>
+  <div class="post-detail">
+    <h1>This is a post page</h1>
+    id: {{ $route.params.id }}
+    <div>
+      <button @click="showPostId">Show post id</button>
+    </div>
+    <p>
+      <RouterLink to="/posts">Back to posts list</RouterLink>
+    </p>
+    <div>
+      <button @click="backToPosts">Back do posts list</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+
+function showPostId() {
+  alert(`ID: ${route.params.id}`)
+}
+
+function backToPosts() {
+  router.push({ name: 'home' })
+}
+
+</script>
---
+++ basics/src/views/PostsView.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+    <ul>
+      <li>
+        <RouterLink to="/postDetail/1">Post 1</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/2">Post 2</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/postDetail/3">Post 3</RouterLink>
+      </li>
+    </ul>
+  </div>
+</template>

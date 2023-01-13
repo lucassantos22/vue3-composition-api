bf350b29b8faa7a204de8abccb90d3d94e792f62
lucassantos22 <lucas.afsantos@hotmail.com>
2023-01-12 22:43:03 -0300

    chore: add router

--- noteballs/src/main.js
+++ noteballs/src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: []
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')

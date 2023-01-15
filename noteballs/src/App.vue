de99f4f728cd61a6591f7dec4ee578e653c336db
lucassantos22 <lucas.afsantos@hotmail.com>
2023-01-15 13:58:01 -0300

    chore: add NavBar

--- noteballs/src/App.vue
+++ noteballs/src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <RouterLink to="/"> Notes </RouterLink> |
-  <RouterLink to="/stats"> Stats </RouterLink>
+  <NavBar ></NavBar>
 
   <RouterView></RouterView>
 </template>
 
+<script setup>
+
+import NavBar from '@/components/Layout/NavBar.vue'
+
+</script>
+
 <style>
-
-@import 'bulma/css/bulma.min.css'
-
+@import 'bulma/css/bulma.min.css';
 </style>

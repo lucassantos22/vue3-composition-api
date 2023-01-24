275f52baec94d79b712b4a0f3959cc98b51d98b4
lucassantos22 <lucas.afsantos@hotmail.com>
2023-01-24 01:41:25 -0300

    chore: add notes store

---
+++ noteballs/src/stores/notes.js
@@ -0,0 +1,21 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useNotesStore = defineStore('notes', () => {
+  const count = ref(0)
+  const notes = ref([
+    {
+        id: 'id1',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.'
+    },
+    {
+        id: 'id2',
+        content: 'Lorem ipsum dolor sit amet, consectetur'
+    }
+  ])
+  function increment() {
+    count.value++
+  }
+
+  return { count, increment, notes }
+})
